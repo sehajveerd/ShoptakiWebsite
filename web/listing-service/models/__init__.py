2d77b42d94200d3dd18215cdaef97026ebd74f37
Yu Gu <yugu0@cmu.edu>
2023-08-16 02:49:26 -0400

    Delete web directory

--- web/listing-service/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .db import db
-from .state import State
-from .property import Property
-from .propertyImage import PropertyImage
-from .project import Project
--- web/user-service/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from .db import db
-# from .userDetail import State
-# from .property import Property
