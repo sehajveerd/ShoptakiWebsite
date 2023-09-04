4b6ca643cc51bbffc8c42268ae0184c169c6c8f0
Rasika Gulhane <gulhanerasika@gmail.com>
2023-09-04 12:52:15 -0400

    Updated Readme instruction and comments added to model.py

--- Models/Recommendation_System/README.md
+++ Models/Recommendation_System/README.md
@@ -1,7 +1,15 @@
 # Property Recomendations sytem
+- Content based Recommendation Syatem
 
 
-### Important factors that are highlighted:
+## Important factors that are highlighted:
+
+#### Dataset from:
+Latest_maintable_copy 
+Latest_maintable_risk_rating_score_copy (result from Risk_Rating_Model)
+
+#### Preprocessed and Model ready dataset are :
+Categories used in Algorithm : zpid,city,homeType,homeStatus,lotAreaUnit,state,newConstructionType,Average Risk Level,bathrooms,bedrooms,livingArea,lotAreaValue,price,rentZestimate,zipcode,taxAssessedValue,datePriceChanged,priceChange,Average Score,investment_risk
 
 Property Type: The type of property, such as residential, single family, multifamily, commercial, land, etc.
 Location: The geographic location of the property, including city, state, neighborhood, and ZIP code.
@@ -12,13 +20,23 @@ Property Age: The age of the property, if available.
 Property Condition: The condition of the property, whether it's new, renovated, or needs repairs.
 Property Category: Categorization of properties based on specific criteria, such as luxury, budget, high-risk, low-risk, etc.
 
+####
+you can modify no. of required recommended properties in
 
 ### How to Run the project
 
-- Clone this repository in your local system.
+- Clone this repository in your system. 
+`
+git clone https://github.com/Shoptaki/real-estate-ai-be.git
+`
 - Install all the libraries mentioned in the 'requirements.txt' inside Models/Recommendation_System folder.
-- Run all cell in RE_DataCleaning python notebook and get the processed clean dataset to use further
-- Dataset file "final_data.csv" auto created 
+`
+pip install -r reqyirements.txt
+`
+- Run all cell in RE_DataCleaning python notebook and get the processed clean dataset to use further (auto create dataset 'final_data.csv)
+
 - Run model.py in Terminal
-- Result is as on now will display in Terminal window
-- To Do: Deploy the model with any Restful API (Django, flask , etc)
+- Result will show 10 properties id. 
+    (You can fetch details of that id and display for recommendation while working on frontend)
+- To Do: Deploy the model with any Restful API (Django, etc)
+
--- Models/Recommendation_System/model.py
+++ Models/Recommendation_System/model.py
@@ -1,57 +1,3 @@
-# from preprocessing import DataPreprocessor
-# from preprocessing import CategoricalEmbedding, RecommendationSystem
-
-
-
-
-# # Load and preprocess data
-# data_preprocessor = DataPreprocessor("/Users/rasikagulhane/Desktop/real-estate-ai-be/Models/Recommendation_System/final_data.csv")
-# normalized_content_features = data_preprocessor.preprocess()
-
-# # Create and convert categorical embeddings
-# categorical_embedding = CategoricalEmbedding(data_preprocessor.data)
-# embeddings = categorical_embedding.create_embeddings()
-# embedded_categorical_features = categorical_embedding.convert_to_embeddings(embeddings)
-
-
-
-# # Build recommendation system
-# # normalized_content_features = data_preprocessor.preprocess()
-# content_features = data_preprocessor.data[['bathrooms', 'bedrooms', 'livingArea', 'lotAreaValue', 'price', 'rentZestimate', 'taxAssessedValue', 'Average Score', 'investment_risk']]
-
-# recommendation_system = RecommendationSystem(normalized_content_features, embedded_categorical_features, "/Users/rasikagulhane/Desktop/real-estate-ai-be/Models/Recommendation_System/final_data.csv")
-
-
-# # Calculate similarity and get recommendations
-# similarities = recommendation_system.calculate_similarity()
-# property_index = 1
-# num_recommendations = 10
-# recommended_zpids = recommendation_system.get_recommendations(property_index, num_recommendations)
-
-# print("Top recommended property indices:")
-# print(recommended_zpids)
-
-
-
-
-
-
-# # Build recommendation system
-# content_features = data_preprocessor.data[['bathrooms', 'bedrooms', 'livingArea', 'lotAreaValue', 'price', 'rentZestimate', 'taxAssessedValue', 'Average Score', 'investment_risk']]
-# recommendation_system = RecommendationSystem(normalized_content_features, embedded_categorical_features, "/Users/rasikagulhane/Desktop/real-estate-ai-be/Models/Recommendation_System/final_data.csv")
-
-# # Calculate similarity and get recommendations
-# similarities = recommendation_system.calculate_similarity()
-# property_index = 1
-# num_recommendations = 10
-# recommended_zpids = recommendation_system.get_recommendations(property_index, num_recommendations)
-
-# print("Top recommended property indices:")
-# print(recommended_zpids)
-
-
-
-
 import pandas as pd
 
 from preprocessing import DataPreprocessor, CategoricalEmbedding, RecommendationSystem
@@ -77,13 +23,14 @@ similarities = cosine_similarity(combined_features)
 recommendation_system = RecommendationSystem(normalized_content_features, embedded_categorical_features, "/Users/rasikagulhane/Desktop/real-estate-ai-be/Models/Recommendation_System/final_data.csv", similarities)
 
 # Property zpid for which you want recommendations
-property_zpid = 2075615600
+property_zpid = 2075615600    #make changes  while deploying as per click of user 
 
 # Calculate similarity for the specified property zpid
 property_similarity = recommendation_system.calculate_similarity(property_zpid)
 print("Similarity for property with zpid:", property_similarity)
 
 # Get recommendations for the specified property zpid
-num_recommendations = 10
+num_recommendations = 10        #change no. of recommendation as per needed to display
 recommended_zpids = recommendation_system.get_recommendations(property_zpid, num_recommendations)
 print("Top recommended property IDs:", recommended_zpids)
+
